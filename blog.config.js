435fcfa0f013b8abccab3d8ec6b374c5192f53cb
shuimuyicom <shuimuyicom@gmail.com>
2025-09-18 15:22:08 +0800

    - 更新博客配置；

--- blog.config.js
+++ blog.config.js
@@ -14,10 +14,10 @@ const BLOG = {
   APPEARANCE: process.env.NEXT_PUBLIC_APPEARANCE || 'auto', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'shuimuyi', // 您的昵称 例如 shuimuyi
-  BIO: process.env.NEXT_PUBLIC_BIO || '产品经理丨哲学硕士｜中华国学探索者', // 作者简介
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '水木易', // 您的昵称 例如 shuimuyi
+  BIO: process.env.NEXT_PUBLIC_BIO || '产品经理，哲学硕士，国学命理探索者', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.shuimuyi.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'shuimuyi, 博客', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '水木易,博客,哲学,国学,命理', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
   BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
